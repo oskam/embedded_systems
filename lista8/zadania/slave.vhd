0e7f3a9952c14d4e85080228c3b7015d5c0f8acc
oskam <oska.magdalena@gmail.com>
2017-05-22 21:18:22 +0200

    zadania

---
+++ lista8/zadania/slave.vhd
@@ -0,0 +1,229 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+--USE IEEE.std_logic_unsigned.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+-----------------------------------------------------------------------
+-- a (working) skeleton template for slave device on 8-bit bus
+--    capable of executing commands sent on the bus in the sequence:
+--    1) device_address (8 bits)
+--		2) cmd_opcode (4 bits) & reserved (4 bits)
+--		3) (optional) cmd_args (8 bits)
+--
+-- currently supported commands:
+-- 	* ID 			[0010] - get device address
+-- 	* DATA_REQ 	[1111] - send current result in the next clockpulse
+-- 	* NOP 		[0000] - don't do anything
+-----------------------------------------------------------------------
+-- debugging information on current state of statemachine and command
+-- executed and input buffer register is given in outputs, vstate,
+-- vcurrent_cmd and vq, respectively
+-----------------------------------------------------------------------
+
+entity slave is
+    generic ( identifier : std_logic_vector (7 downto 0) := "10101010" );
+    Port ( conn_bus : inout  STD_LOGIC_VECTOR (7 downto 0);
+           clk : in  STD_LOGIC;
+			  state : out STD_LOGIC_VECTOR (5 downto 0);
+			  vq : out std_logic_vector (7 downto 0);
+			  vcurrent_cmd : out std_logic_vector(3 downto 0)
+			  );
+end slave;
+
+architecture Behavioral of slave is
+
+-- statemachine definitions
+type state_type is (IDLE, CMD, RUN);
+signal current_s : state_type := IDLE;
+signal next_s : state_type := IDLE;
+-- for debugging entity's state
+signal vstate : std_logic_vector(5 downto 0) := (others => '0');
+
+-- command definitions
+type cmd_type is (NOP, ADD, ID, CRC, RST, HENC, HDEC, DATA_REQ);
+attribute enum_encoding: string;
+attribute enum_encoding of cmd_type: type is
+				"0000 0001 0010 0011 0100 0101 0110 1111";
+signal current_cmd : cmd_type := NOP;
+
+-- input buffer
+signal q : std_logic_vector (7 downto 0) := (others => '0');
+
+-- for storing results and indicating it is to be sent to bus
+signal result_reg : std_logic_vector (7 downto 0) := (others => '0');
+signal sending : std_logic := '0';
+
+function nextCRC (
+    data_in		: std_logic_vector(7 downto 0);
+    prevCRC 	: std_logic_vector(7 downto 0)
+) return std_logic_vector is
+    variable D	      : std_logic_vector(7 downto 0);
+    variable C	      : std_logic_vector(7 downto 0);
+    variable newCRC  : std_logic_vector(7 downto 0);
+begin
+    D := data_in;
+    C := prevCRC;
+
+    newCRC(0) := D(7) xor D(6) xor D(0) xor C(0) xor C(6) xor C(7);
+    newCRC(1) := D(6) xor D(1) xor D(0) xor C(0) xor C(1) xor C(6);
+    newCRC(2) := D(6) xor D(2) xor D(1) xor D(0) xor C(0) xor C(1)
+                            xor C(2) xor C(6);
+    newCRC(3) := D(7) xor D(3) xor D(2) xor D(1) xor C(1) xor C(2)
+                            xor C(3) xor C(7);
+    newCRC(4) := D(4) xor D(3) xor D(2) xor C(2) xor C(3) xor C(4);
+    newCRC(5) := D(5) xor D(4) xor D(3) xor C(3) xor C(4) xor C(5);
+    newCRC(6) := D(6) xor D(5) xor D(4) xor C(4) xor C(5) xor C(6);
+    newCRC(7) := D(7) xor D(6) xor D(5) xor C(5) xor C(6) xor C(7);
+
+    return newCRC;
+end nextCRC;
+
+function hamming_encoder (
+    data_in : std_logic_vector(3 downto 0)
+) return std_logic_vector is
+    variable data_out : std_logic_vector(7 downto 0);
+begin
+    data_out(0) := data_in(0) xor data_in(1) xor data_in(3);
+    data_out(1) := data_in(0) xor data_in(2) xor data_in(3);
+    data_out(2) := data_in(0);
+    data_out(3) := data_in(1) xor data_in(2) xor data_in(3);
+    data_out(4) := data_in(1);
+    data_out(5) := data_in(2);
+    data_out(6) := data_in(3);
+
+    data_out(7) := '0';
+
+    return data_out;
+end hamming_encoder;
+
+function hamming_decoder (
+    data_in	: std_logic_vector(6 downto 0)
+) return std_logic_vector is
+    variable data_out : std_logic_vector(7 downto 0);
+    variable checker : std_logic_vector(2 downto 0);
+begin
+    checker(0) := data_in(6) xor data_in(4) xor data_in(2) xor data_in(0);
+    checker(1) := data_in(6) xor data_in(5) xor data_in(2) xor data_in(1);
+    checker(2) := data_in(6) xor data_in(5) xor data_in(4) xor data_in(3);
+
+    if checker = "011" then
+        data_out(0) := not data_in(2);
+    else
+        data_out(0) := data_in(2);
+    end if;
+
+    if checker = "101" then
+        data_out(1) := not data_in(4);
+    else
+        data_out(1) := data_in(4);
+    end if;
+
+    if checker = "110" then
+        data_out(2) := not data_in(5);
+    else
+        data_out(2) := data_in(5);
+    end if;
+
+    if checker = "111" then
+        data_out(3) := not data_in(6);
+    else
+        data_out(3) := data_in(6);
+    end if;
+
+    data_out(7 downto 4) := "0000";
+
+    return data_out;
+end hamming_decoder;
+
+begin
+
+stateadvance: process(clk)
+begin
+  if rising_edge(clk)
+  then
+    q  <= conn_bus;
+    current_s <= next_s;
+  end if;
+end process;
+
+
+nextstate: process(current_s,q)
+  variable fourbit : std_logic_vector(3 downto 0) := "0000";
+begin
+
+ case current_s is
+   when IDLE =>
+		vstate <= "000001";		-- set for debugging
+		if q = identifier and sending /= '1'
+		then
+	      next_s <= CMD;
+		else
+			next_s <= IDLE;
+		end if;
+		sending <= '0';
+	when CMD =>
+		vstate <= "000010";
+		-- command decode
+		fourbit := q(7 downto 4);
+		case fourbit is
+			when "0000" => current_cmd <= NOP;
+			when "0001" => current_cmd <= ADD;
+			when "0010" => current_cmd <= ID;
+			when "0011" => current_cmd <= CRC;
+			when "0100" => current_cmd <= RST;
+			when "0101" => current_cmd <= HENC;
+			when "0110" => current_cmd <= HDEC;
+			when "1111" => current_cmd <= DATA_REQ;
+			when others => current_cmd <= NOP;
+		end case;
+		next_s <= RUN;
+	when RUN =>
+		vstate <= "000100";
+		-- determine action based on currend_cmd state
+		case current_cmd is
+			when NOP
+				=> result_reg <= result_reg;
+			when ID
+				=> result_reg <= identifier;
+			when DATA_REQ
+				=> sending <= '1';
+            when ADD
+                => result_reg <=std_logic_vector(unsigned(result_reg) + unsigned(q));
+            when CRC
+                => result_reg <= nextCRC(q, result_reg);
+            when RST
+                => result_reg <= "00000000";
+            when HENC
+                => result_reg <= "00000000";
+                result_reg <= hamming_encoder(q(3 downto 0));
+            when HDEC
+                => result_reg <= "00000000";
+                result_reg <= hamming_decoder(q(6 downto 0));
+			--
+			-- here other commands execution
+			--
+			when others
+				=> result_reg <= result_reg;
+		end case;
+		next_s <= IDLE;
+   when others =>
+		vstate <= "111111";
+		next_s <= IDLE;
+   end case;
+end process;
+
+
+-- tri-state bus
+conn_bus <= result_reg when sending = '1' else "ZZZZZZZZ";
+
+-- output debugging signals
+state <= vstate;
+vq    <= q;
+with current_cmd select
+    vcurrent_cmd <= "0001" when ADD,
+                    "0010" when ID,
+                    "0011" when CRC,
+                    "1111" when DATA_REQ,
+                    "0000" when others;
+
+end Behavioral;
---
+++ lista8/zadania/slave_tb.vhd
@@ -0,0 +1,377 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+--USE IEEE.std_logic_unsigned.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+USE ieee.numeric_std.ALL;
+
+ENTITY slave_tb IS
+END slave_tb;
+
+ARCHITECTURE behavior OF slave_tb IS
+
+    -- Component Declaration for the Unit Under Test (UUT)
+
+    COMPONENT slave
+    generic ( identifier : std_logic_vector (7 downto 0) );
+    PORT(
+         conn_bus : INOUT  std_logic_vector(7 downto 0);
+         clk : IN  std_logic;
+			state : out STD_LOGIC_VECTOR (5 downto 0);
+			vq : out std_logic_vector (7 downto 0);
+			vcurrent_cmd : out std_logic_vector(3 downto 0)
+        );
+    END COMPONENT;
+
+
+    --Inputs
+    signal clk : std_logic := '0';
+    signal n_clk : std_logic := '1';
+
+    --BiDirs
+    signal conn_bus : std_logic_vector(7 downto 0) := (others => 'Z');
+
+
+    -- outputs from UUT for debugging
+    signal state : std_logic_vector(5 downto 0);
+    signal vq : std_logic_vector (7 downto 0);
+    signal current_cmd : std_logic_vector (3 downto 0);
+
+    -- outputs from UUT for debugging
+    signal state2 : std_logic_vector(5 downto 0);
+    signal vq2 : std_logic_vector (7 downto 0);
+    signal current_cmd2 : std_logic_vector (3 downto 0);
+
+    -- outputs from UUT for debugging
+    signal state3 : std_logic_vector(5 downto 0);
+    signal vq3 : std_logic_vector (7 downto 0);
+    signal current_cmd3 : std_logic_vector (3 downto 0);
+
+    -- Clock period definitions
+    constant clk_period : time := 10 ns;
+
+BEGIN
+
+	-- Instantiate the Unit Under Test (UUT)
+    uut: slave
+	GENERIC MAP (identifier => "10101010")
+	PORT MAP (
+        conn_bus => conn_bus,
+        clk => clk,
+        state => state,
+        vq => vq,
+        vcurrent_cmd => current_cmd
+    );
+
+    uut2: slave
+	GENERIC MAP (identifier => "01010101")
+	PORT MAP (
+        conn_bus => conn_bus,
+        clk => clk,
+        state => state2,
+        vq => vq2,
+        vcurrent_cmd => current_cmd2
+    );
+
+    uut3: slave
+	GENERIC MAP (identifier => "11001100")
+	PORT MAP (
+        conn_bus => conn_bus,
+        clk => n_clk,
+        state => state3,
+        vq => vq3,
+        vcurrent_cmd => current_cmd3
+    );
+
+    -- Clock process definitions
+    clk_process :process
+    begin
+    	clk <= '0';
+        n_clk <= '1';
+    	wait for clk_period/2;
+    	clk <= '1';
+    	n_clk <= '0';
+    	wait for clk_period/2;
+    end process;
+
+
+    -- Stimulus process
+    stim_proc: process
+    begin
+        -- hold reset state for 100 ns.
+        wait for 100 ns;
+
+        -- ID AND DATA REQUEST
+
+		-- address1
+		conn_bus <= "10101010";
+		wait for clk_period;
+		-- CMD: id
+		conn_bus <= "00100000";
+		wait for clk_period*2;
+		-- address1
+		conn_bus <= "10101010";
+		wait for clk_period;
+		-- CMD: data_req
+		conn_bus <= "11110000";
+		wait for clk_period;
+		-- this is needed to allow writing on bus by slave
+		conn_bus <= "ZZZZZZZZ";
+        wait for clk_period;
+        assert conn_bus = "10101010" report "bad ID1";
+        wait for clk_period;
+
+        -- address2
+		conn_bus <= "01010101";
+		wait for clk_period;
+		-- CMD: id
+		conn_bus <= "00100000";
+		wait for clk_period*2;
+		-- address2
+		conn_bus <= "01010101";
+		wait for clk_period;
+		-- CMD: data_req
+		conn_bus <= "11110000";
+		wait for clk_period;
+		-- this is needed to allow writing on bus by slave
+		conn_bus <= "ZZZZZZZZ";
+        wait for clk_period;
+        assert conn_bus = "01010101" report "bad ID2";
+        wait for clk_period;
+
+        -- address3
+		conn_bus <= "11001100";
+		wait for clk_period;
+		-- CMD: id
+		conn_bus <= "00100000";
+		wait for clk_period*2;
+		-- address3
+		conn_bus <= "11001100";
+		wait for clk_period;
+		-- CMD: data_req
+		conn_bus <= "11110000";
+		wait for clk_period;
+		-- this is needed to allow writing on bus by slave
+		conn_bus <= "ZZZZZZZZ";
+        wait for clk_period;
+        assert conn_bus = "11001100" report "bad ID3";
+        wait for clk_period;
+
+        -- ADD
+
+		-- address1
+		conn_bus <= "10101010";
+		wait for clk_period;
+		-- CMD: add
+		conn_bus <= "00010000";
+		wait for clk_period;
+		-- add operands
+		conn_bus <= "00000101"; -- 0xAA + 0x05 = 0xAF
+		wait for 2*clk_period;
+        -- address1
+		conn_bus <= "10101010";
+		wait for clk_period;
+		-- CMD: data_req
+		conn_bus <= "11110000";
+		wait for clk_period;
+		-- this is needed to allow writing on bus by slave
+		conn_bus <= "ZZZZZZZZ";
+        wait for clk_period;
+        assert conn_bus = x"AF" report "bad ADD";
+        wait for clk_period;
+
+        -- CRC
+
+		-- address2
+		conn_bus <= "01010101";
+		wait for clk_period;
+		-- CMD: reset
+		conn_bus <= "01000000";
+		wait for 2*clk_period;
+
+        for I in 0 to 7 loop
+
+            -- address2
+    		conn_bus <= "01010101";
+    		wait for clk_period;
+    		-- CMD: crc
+    		conn_bus <= "00110000";
+    		wait for clk_period;
+            -- operands
+            conn_bus <= "01100110";
+            -- calc CRC from 0x66
+            -- X"35",
+        	-- X"be",
+        	-- X"06",
+        	-- X"27",
+        	-- X"c0",
+        	-- X"7b",
+        	-- X"53",
+        	-- X"8b"
+            wait for 2*clk_period;
+
+        end loop;
+
+        -- address2
+		conn_bus <= "01010101";
+		wait for clk_period;
+		-- CMD: data_req
+		conn_bus <= "11110000";
+		wait for clk_period;
+		-- this is needed to allow writing on bus by slave
+		conn_bus <= "ZZZZZZZZ";
+        wait for clk_period;
+        assert conn_bus = x"8b" report "bad CRC";
+        wait for clk_period;
+
+        -- HAMMING ENCODER
+
+        -- address1
+        conn_bus <= "10101010";
+        wait for clk_period;
+        -- CMD: hamming_encoder
+        conn_bus <= "01010000";
+        wait for clk_period;
+        -- operands
+        conn_bus <= "00000110"; -- hamming_encoder(0110) = 00110011 = 0x33
+        wait for 2*clk_period;
+        -- address1
+		conn_bus <= "10101010";
+		wait for clk_period;
+		-- CMD: data_req
+		conn_bus <= "11110000";
+		wait for clk_period;
+		-- this is needed to allow writing on bus by slave
+		conn_bus <= "ZZZZZZZZ";
+        wait for clk_period;
+        assert conn_bus = x"33" report "bad HENC";
+        wait for clk_period;
+
+        -- HAMMING DECODER
+
+        -- address2
+        conn_bus <= "01010101";
+        wait for clk_period;
+        -- CMD: hamming_decoder
+        conn_bus <= "01100000"; -- 01100110 from above with one bit error
+        wait for clk_period;
+        -- operands
+        conn_bus <= "01110011"; -- haming_decoder(1110011) = 0110 = 0x06
+        wait for 2*clk_period;
+        -- address2
+		conn_bus <= "01010101";
+		wait for clk_period;
+		-- CMD: data_req
+		conn_bus <= "11110000";
+		wait for clk_period;
+		-- this is needed to allow writing on bus by slave
+		conn_bus <= "ZZZZZZZZ";
+        wait for clk_period;
+        assert conn_bus = x"06" report "bad HDEC";
+        wait for clk_period;
+
+        -- RESET
+
+        -- address1
+		conn_bus <= "10101010";
+		wait for clk_period;
+		-- CMD: reset
+		conn_bus <= "01000000";
+		wait for 2*clk_period;
+        -- address1
+		conn_bus <= "10101010";
+		wait for clk_period;
+		-- CMD: data_req
+		conn_bus <= "11110000";
+		wait for clk_period;
+		-- this is needed to allow writing on bus by slave
+		conn_bus <= "ZZZZZZZZ";
+        wait for clk_period;
+        assert conn_bus = x"00" report "1 not reset";
+        wait for clk_period;
+
+        -- address2
+		conn_bus <= "01010101";
+		wait for clk_period;
+		-- CMD: reset
+		conn_bus <= "01000000";
+		wait for 2*clk_period;
+        -- address2
+		conn_bus <= "01010101";
+		wait for clk_period;
+		-- CMD: data_req
+		conn_bus <= "11110000";
+		wait for clk_period;
+		-- this is needed to allow writing on bus by slave
+		conn_bus <= "ZZZZZZZZ";
+        wait for clk_period;
+        assert conn_bus = x"00" report "2 not reset";
+        wait for clk_period;
+
+        -- address3
+		conn_bus <= "11001100";
+		wait for clk_period;
+		-- CMD: reset
+		conn_bus <= "01000000";
+		wait for 2*clk_period;
+        -- address3
+		conn_bus <= "11001100";
+		wait for clk_period;
+		-- CMD: data_req
+		conn_bus <= "11110000";
+		wait for clk_period;
+		-- this is needed to allow writing on bus by slave
+		conn_bus <= "ZZZZZZZZ";
+        wait for clk_period;
+        assert conn_bus = x"00" report "3 not reset";
+        wait for clk_period;
+
+        -- HAMMING ENCODER/DECODER without master
+
+        -- address3
+        conn_bus <= "11001100";
+        wait for clk_period;
+        -- CMD: hamming_encoder
+        conn_bus <= "01010000";
+        wait for clk_period;
+        -- operands
+        conn_bus <= "00000110"; -- hamming_encoder(0110) = 00110011 = 0x33
+        wait for clk_period;
+
+        -- pause for clearance
+        conn_bus <= "ZZZZZZZZ";
+        wait for clk_period;
+
+        -- address3
+		conn_bus <= "11001100";
+		wait for clk_period/2;
+        -- address1
+		conn_bus <= "10101010";
+		wait for clk_period/2;
+		-- CMD: data_req
+		conn_bus <= "11110000";
+		wait for clk_period/2;
+        -- CMD: hamming_decoder
+        conn_bus <= "01100000"; -- result from above
+        wait for clk_period/2;
+		-- this is needed to allow writing on bus by slave
+		conn_bus <= "ZZZZZZZZ";
+        wait for clk_period*2;
+
+        -- address1
+		conn_bus <= "10101010";
+		wait for clk_period;
+		-- CMD: data_req
+		conn_bus <= "11110000";
+		wait for clk_period;
+		-- this is needed to allow writing on bus by slave
+		conn_bus <= "ZZZZZZZZ";
+        wait for clk_period;
+        assert conn_bus = x"06" report "bad HDEC";
+        wait for clk_period;
+
+      wait;
+   end process;
+
+END;
